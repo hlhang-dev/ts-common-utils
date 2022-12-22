546709f71a36babe64a9a1fa93afdd58712fd12c
hlhang-dev <hlhang@outlook.com>
2022-12-22 14:34:30 +0800

    🔥: add random str

--- src/utils/StringUtils.ts
+++ src/utils/StringUtils.ts
@@ -1,4 +1,51 @@
 export class StringUtils {
+
+  public static getRandomStr(): string {
+    let chars = [
+      '0',
+      '1',
+      '2',
+      '3',
+      '4',
+      '5',
+      '6',
+      '7',
+      '8',
+      '9',
+      'A',
+      'B',
+      'C',
+      'D',
+      'E',
+      'F',
+      'G',
+      'H',
+      'I',
+      'J',
+      'K',
+      'L',
+      'M',
+      'N',
+      'O',
+      'P',
+      'Q',
+      'R',
+      'S',
+      'T',
+      'U',
+      'V',
+      'W',
+      'X',
+      'Y',
+      'Z'
+    ]
+    let res = ''
+    for (let i = 0; i < 10; i++) {
+      let id = Math.ceil(Math.random() * 35)
+      res += chars[id]
+    }
+    return res
+  }
   public static isEmpty<T>(str: T): boolean {
     return (str && typeof str === 'string' && str.length === 0)
   }
