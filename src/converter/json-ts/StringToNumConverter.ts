0269ff3ebe5de2363eee5c49619c47759d415265
hlhang-dev <hlhang-dev@outlook.com>
2023-07-31 10:38:29 +0800

    ✨: add number uuid generator

--- src/converter/json-ts/StringToNumConverter.ts
+++ src/converter/json-ts/StringToNumConverter.ts
@@ -1,11 +1,12 @@
-import { JsonConverter,JsonCustomConvert } from 'json2typescript'
+import { JsonConverter, JsonCustomConvert } from 'json2typescript'
 
 @JsonConverter
 export class StringToNumConverter implements JsonCustomConvert<number> {
-  serialize (data: number): any {
-    return null
-  }
-  deserialize (data: string): number {
-    return (parseInt(data,10) || 0)
-  }
+    serialize(data: number): any {
+        return null
+    }
+
+    deserialize(data: string): number {
+        return Number(data)
+    }
 }
---
+++ src/utils/NumberUUIDGenerator.ts
@@ -0,0 +1,45 @@
+class NumberUUIDGenerator {
+    private static instance: NumberUUIDGenerator
+    private digits: string
+
+    private constructor() {
+        this.digits = '0123456789'
+    }
+
+    public static getInstance(): NumberUUIDGenerator {
+        if (!NumberUUIDGenerator.instance) {
+            NumberUUIDGenerator.instance = new NumberUUIDGenerator()
+        }
+        return NumberUUIDGenerator.instance
+    }
+
+    private generateRandomNumber(length: number): string {
+        let result = ''
+
+        for (let i = 0; i < length; i++) {
+            result += this.digits.charAt(Math.floor(Math.random() * this.digits.length))
+        }
+
+        return result
+    }
+
+    private shuffleString(str: string): string {
+        let array = str.split('')
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]]
+        }
+        return array.join('')
+    }
+
+    public generateCustomUUID(length: number = 32): string {
+        const timestamp = Date.now().toString()
+        const randomNumber = this.generateRandomNumber(length - timestamp.length)
+        const mixedString = this.shuffleString(timestamp + randomNumber)
+        return mixedString
+    }
+}
+
+export {
+    NumberUUIDGenerator
+}
